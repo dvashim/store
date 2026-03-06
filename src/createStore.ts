07882a33ebf1667d46d2bdab18e59bed4b0c6e6b
Aleksei Reznichenko <rez.alex@gmail.com>
2026-03-06 17:25:38 +0200

    refactor: adjust createStore function signatures for clarity and consistency

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,8 +1,8 @@
 import { Store } from './Store'
 
-function createStore<T>(state: T): Store<T>
 function createStore<T = undefined>(): Store<T | undefined>
-function createStore<T>(state?: T | undefined): Store<T | undefined> {
+function createStore<T>(state: T): Store<T>
+function createStore<T>(state?: T): Store<T | undefined> {
   return new Store(state)
 }
 
